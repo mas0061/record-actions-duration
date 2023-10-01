25d3f1b3454276c0c5dfe0f3802b7c26466ae979
mas0061 <mas0061@users.noreply.github.com>
2023-10-01 12:08:18 +0900

    build: add bundler

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const core = require('@actions/core')
 
-try {
-  const repositoryName = core.getInput('repository_name')
-  const workflowName = core.getInput('workflow_name')
+async function run() {
+  try {
+    const repositoryName = core.getInput('repository_name')
+    const workflowName = core.getInput('workflow_name')
 
-  console.log(repositoryName)
-  console.log(workflowName)
-} catch(error) {
-  core.setFailed(error.message)
+    console.log(repositoryName)
+    console.log(workflowName)
+  } catch(error) {
+    core.setFailed(error.message)
+  }
 }
-
