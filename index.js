8914650e1b17e0ccad7d403dbea3408e605233c1
mas0061 <mas0061@users.noreply.github.com>
2023-10-01 16:00:24 +0900

    feat: add duration

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
+
+const format = require('date-fns/format')
+const parseISO = require('date-fns/parseISO')
 
 async function run() {
   try {
     const repositoryName = core.getInput('repository_name')
     const workflowName = core.getInput('workflow_name')
 
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+
     core.info(repositoryName)
     core.info(workflowName)
+
+    const { data: actionsRun } = await octokit.rest.actions.listWorkflowRuns({
+      owner: repositoryName.split('/')[0],
+      repo: repositoryName.split('/')[1],
+      workflow_id: workflowName
+    })
+    const runs = actionsRun.workflow_runs
+
+    for (const run of runs) {
+      const { data: duration } = await octokit.rest.actions.getWorkflowRunUsage({
+        owner: repositoryName.split('/')[0],
+        repo: repositoryName.split('/')[1],
+        run_id: run.id
+      })
+      core.info(`${run.id}: ${duration.run_duration_ms}ms: ${format(parseISO(run.created_at), 'yyyy/MM/dd HH:mm:ss')}`)
+    }
   } catch(error) {
+    core.error(error)
     core.setFailed(error.message)
   }
 }
