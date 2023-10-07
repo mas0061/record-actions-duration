8a19435a87c2e7d823b7a068805d26cf79cacc42
mas0061 <mas0061@users.noreply.github.com>
2023-10-07 22:10:20 +0900

    fix: typo

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const repositoryName = core.getInput('repository_name')
 // repositoryNameが / で2つの文字列に分割でき、それぞれの文字列長が1以上ない場合はエラーになる
 if (!repositoryName || repositoryName.split('/').length !== 2 || repositoryName.split('/').some((name) => name.length === 0)) {
   core.setFailed('Invalid repository name')
-  proces.exit(1)
+  process.exit(1)
 }
 
 const [owner, repo] = repositoryName.split('/')
